aedc62790b9ea0fa4388bc21a71082730d76d5c2
Olena Parakonna <166116539+Parakonna@users.noreply.github.com>
2024-12-10 21:33:29 +0100

    mongoos

---
+++ src/db/models/Contact.js
@@ -0,0 +1,36 @@
+import { Schema, model } from 'mongoose';
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    isFavourite: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: ['personal'],
+    },
+  },
+  {
+    timestamps: true,
+    createdAt: Date.now,
+    updatedAt: Date.now,
+  },
+);
+
+const ContactCollection = model('contact', contactSchema);
+export default ContactCollection;
